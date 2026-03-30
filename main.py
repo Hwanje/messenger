efe7f88fa77e7dd9c289883298c886dcc5f7b37e
Axion_Hwanje <108977628+Hwanje@users.noreply.github.com>
2026-03-30 22:53:00 +0900

    image

--- main.py
+++ main.py
@@ -26,28 +26,36 @@ async def connect(sid, environ):
 async def create_room(sid, data):
     room_name = data.get('room')
     if not room_name: return
-    if room_name not in rooms_db:
-        rooms_db[room_name] = pyotp.random_base32()
     
-    await sio.enter_room(sid, room_name)
+    # [수정] 방 이름 중복 체크
+    if room_name in rooms_db:
+        await sio.emit('join_fail', {'msg': "이미 존재하는 방 이름입니다. 다른 이름을 사용하세요."}, to=sid)
+        return
+    
+    # 방 생성 및 시크릿 키 할당
+    rooms_db[room_name] = pyotp.random_base32()
     totp = pyotp.TOTP(rooms_db[room_name], interval=60)
-    await sio.emit('display_otp', {'code': totp.now()}, room=room_name)
-    await sio.emit('join_success', {'room': room_name}, to=sid)
+    
+    # 방장에게 OTP 번호 전송 (아직 입장은 시키지 않음)
+    await sio.emit('display_otp', {'code': totp.now()}, to=sid)
+    await sio.emit('create_success', {'room': room_name}, to=sid)
 
 @sio.event
 async def join_with_otp(sid, data):
     room = data.get('room')
     otp_code = data.get('code')
+    
     if room in rooms_db:
         totp = pyotp.TOTP(rooms_db[room], interval=60)
         if totp.verify(otp_code):
+            # [보안] OTP 검증 성공 시에만 실제로 방에 입장(enter_room) 시킴
             await sio.enter_room(sid, room)
             await sio.emit('join_success', {'room': room}, to=sid)
             await sio.emit('display_otp', {'code': totp.now()}, to=sid)
         else:
             await sio.emit('join_fail', {'msg': "OTP가 일치하지 않습니다."}, to=sid)
     else:
-        await sio.emit('join_fail', {'msg': "방을 찾을 수 없습니다."}, to=sid)
+        await sio.emit('join_fail', {'msg': "존재하지 않는 방입니다."}, to=sid)
 
 @sio.event
 async def set_nickname(sid, data):
@@ -68,9 +76,17 @@ async def send_secure_msg(sid, data):
     nickname = user_sessions.get(sid, "익명")
     room = data.get('room')
     msg = data.get('msg')
+    msg_type = data.get('type', 'text') # 텍스트인지 파일인지 구분
+    
     if room and msg:
-        await sio.emit('receive_secure_msg', {'msg': msg, 'sender': nickname}, room=room)
-
+        await sio.emit('receive_secure_msg', {
+            'msg': msg, 
+            'sender': nickname,
+            'type': msg_type,
+            'fileName': data.get('fileName'),
+            'fileType': data.get('fileType')
+        }, room=room)
+        
 @sio.event
 async def leave_room(sid, data):
     room = data.get('room')
